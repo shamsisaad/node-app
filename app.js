8e99cfacc0b0de73780ace1f588b05a3bf5706ab
shamsisaad <engrsaadshamsi@gmail.com>
2023-10-27 22:47:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 
-
+const form = require("./routes/form")
 
 app.use((req, res, next) => {
     console.log(req.url);
     next();
 });
 
-app.use((req, res, next) => {
-    res.send(req.url);
-});
+app.use("/form", form);
 
 app.listen(3000)
