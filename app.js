7d6b3513cf0b884c4526f0b6fce73ad27f743f72
shamsisaad <engrsaadshamsi@gmail.com>
2023-10-27 21:57:27 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
+const app = express();
 
-const filePath = path.join(process.cwd(), "data.txt");
-const server = http.createServer((req, res) => {
-   if (req.url === '/') {
-    res.write("Hello World");
-    res.end();
-} else if (req.url === '/form') {
-    res.setHeader("content-type", "text/html");
-    res.write("<form action='/submit' method='POST'><input name='data' /> <input name='data2' /><button>Submit</button> </form>");
-    res.end();
-} else if (req.url === '/submit') {
-    let data = "";
-    req.on("data", chunk => data+=chunk);
-    req.on("end", () =>{
-        fs.readFile(filePath, "utf-8", (_, fileData) => {
-            const newData = fileData + "\n" + data;
-            fs.writeFile(filePath, newData, () => {
-            res.write("Data Received");
-            res.end();
-        });
-        })
-        
-    //  console.log(data)
-        
-    });
 
-}
- else {
-    res.write("404 - Page Not Found");
-    res.end();
-
-}
-});
-server.listen(3000)
+app.listen(3000)
