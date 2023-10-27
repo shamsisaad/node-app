b2dcf338648d68ff2c7cc58e89df9d1b6a6133e4
shamsisaad <engrsaadshamsi@gmail.com>
2023-10-27 22:26:20 +0500

    Create routes folder

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
+    console.log(req.url);
+    next();
+});
+
+app.use((req, res, next) => {
     res.send(req.url);
 });
 
