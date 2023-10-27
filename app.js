4e59e32effd5713bcbb97b2284debc226f99c6a1
shamsisaad <engrsaadshamsi@gmail.com>
2023-10-27 22:35:50 +0500

    Update form.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+
+
 app.use((req, res, next) => {
     console.log(req.url);
     next();
--- routes/form.js
+++ routes/form.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-
+router.get("/", (req, res) => {
+    res.send(`
+        <form>
+            <input name="data" />
+            <button>Submit</button>
+        </form>
+    `);
+})
 
 
 
