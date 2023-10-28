8fb40b1e439db81c2064e52c13719b9c937d4cc8
shamsisaad <engrsaadshamsi@gmail.com>
2023-10-28 22:30:31 +0500

    Update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require("path");
 
 const form = require("./routes/form")
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(process.cwd(), "publid")))
 
 app.use((req, res, next) => {
     req.data = "shamsi";
