30646d066c785a4c1877e66447093a3a5b6f085b
shamsisaad <engrsaadshamsi@gmail.com>
2023-11-01 22:34:35 +0500

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -3,16 +3,13 @@ const path = require("path");
 
 const router = express.Router();
 
-router.post("/", (req, res, next) => {
+router.post("/form", (req, res, next) => {
     res.send(req.body);
 });
 
-router.get("/", (req, res, next) => {
+router.get("/form", (req, res, next) => {
     res.sendFile(path.join(process.cwd(), "views", "form.html"));
 });
 
-// router.post("/submit", (req, res) => {
-    // res.send(req.body);
-// });
 
 module.exports = router;
