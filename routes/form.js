783094554a984e402064f1d073d57cf0a619da67
shamsisaad <engrsaadshamsi@gmail.com>
2023-11-01 22:19:29 +0500

    Update form.js

--- routes/form.js
+++ routes/form.js
@@ -2,12 +2,17 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
-router.get("/", (req, res) => {
+
+router.post("/", (req, res, next) => {
+    res.send(req.body);
+});
+
+router.get("/", (req, res, next) => {
     res.sendFile(path.join(process.cwd(), "views", "form.html"));
 });
 
-router.post("/submit", (req, res) => {
-    res.send(req.body);
-});
+// router.post("/submit", (req, res) => {
+    // res.send(req.body);
+// });
 
 module.exports = router;
